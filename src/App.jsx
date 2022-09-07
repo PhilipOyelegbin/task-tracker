73d00273e8e2980b5b4f5dc3e87d62b3fbe4b9cf
tosmel2 <adewaletosin0808@gmail.com>
2022-09-07 14:18:09 +0100

    task tracker signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import SharedLayout from './components/SharedLayout';
 import Landing from './pages/landing/Landing'
 import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<SharedLayout/>}>
         <Route index element={<Landing/>}/>
         <Route path='login' element={<Login/>}/>
+        <Route path='signup' element={<Signup/>}/>
       </Route>
     </Routes>
     </>
---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+// import TGLogo  from '../../assets/TGLogo.jpg';
+import '../signup/signup.css'
+
+const Signup = () => {
+  let navigate = useNavigate();
+  const [type , setType] = useState({visible: false, input:"password"});
+  function tooglePassword() {
+    setType((value)=>{
+      return {
+        ...value,
+        visible : !value.visible,
+        input : !value.visible ? "text" : "password"
+      }
+    })
+  }          
+
+  return (
+    <>
+      <div className='signup-form-container'>
+        {/* <img src={TGLogo} alt="TGLogo" className='signup-logo'/> */}
+        <h1 className='signup-heading'>Start Your Free Trial</h1>
+        <form onSubmit={(e)=>e.preventDefault()} className='reg-form'>
+        {/* <h1>Welcome to Lilies !</h1> */}
+          <div className='input-box'>
+            <label>First Name</label>
+            <input type="text" placeholder='Enter First Name' />
+          </div>
+          <div className='input-box'>
+            <label>Last Name</label>
+            <input type="text" placeholder='Enter Last Name' />
+          </div>
+          <div className='input-box'>
+          <label>Email Address</label>
+            <input type="email" placeholder='Enter Email' />
+          </div>
+          <div className='input-box'>
+            <label>Password</label>
+            <input type={type.input} placeholder='Enter Password' />
+            <i className={`show-btn ${type.visible ? "fas fa-eye" : "fas fa-eye-slash"}`} onClick={tooglePassword}></i>
+          </div>
+          <div className='input-box'>
+            <label>Confirm Password</label>
+            <input type={type.input} placeholder='Confirm Password' />
+            <i className={`show-btn ${type.visible ? "fas fa-eye" : "fas fa-eye-slash"}`} onClick={tooglePassword}></i>
+          </div>
+          <div className='input-box'>
+            <button className='signup-btn' onClick={()=>navigate('/login')}>Sign Up</button>
+          </div>
+          <div>
+            <p>Already have an account ? <span style={{color:'#DD6B20', cursor:'pointer', fontWeight:'bold'}} onClick={()=>navigate('/login')}>Login</span></p>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default Signup
