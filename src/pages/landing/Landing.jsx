b6edb3f357dacdc552c5c25c44a4b25ffc451af5
Excel <edehexcel2000@gmail.com>
2022-09-07 21:04:48 +0100

    added second sectioon

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,12 +1,20 @@
+import Footer from "./Footer"
 import Hero from "./Hero"
+import NewsLetter from "./NewsLetter"
+import Projects from "./Projects"
 import Section2 from "./Section2"
+import Testimonials from "./Testimonials"
 
 const Landing = () => {
   return (
-    <>
+    <div style={{backgroundColor:'#5C5757'}}>
         <Hero/>
         <Section2/>
-    </>
+        <Projects/>
+        <Testimonials/>
+        <NewsLetter/>
+        <Footer/>
+    </div>
   )
 }
 
